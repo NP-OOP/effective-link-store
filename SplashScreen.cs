622d73c80077caab4bf5521e60f634349fc2a8cb
NP <ar112705@fhss.sjp.ac.lk>
2025-03-06 13:38:12 +0530

    SplashScreen .cs file | Added via file upload

---
+++ SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkManager.Forms
+{
+    public partial class SplashScreen: Form
+    {
+        public SplashScreen()
+        {
+            this.SuspendLayout();
+            // 
+            // SplashScreen
+            // 
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Name = "SplashScreen";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+        }
+    }
+}
